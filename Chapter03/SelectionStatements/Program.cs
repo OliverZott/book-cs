7f5b975bf581803589e5f58b2a4625a0134261ed
Oliver Zott <zott_oliver@web.de>
2021-03-21 18:01:10 +0100

    Adding switch-case example

---
+++ Chapter03/SelectionStatements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        A_label:
+            var number = (new Random()).Next(1, 7);
+            System.Console.WriteLine($"Next random number: {number}");
+            switch (number)
+            {
+                case 1:
+                    System.Console.WriteLine("One");
+                    break;
+                case 2:
+                    System.Console.WriteLine("Two");
+                    break;
+                case 3:
+                case 4:
+                    System.Console.WriteLine("Four");
+                    goto case 1;
+                case 5:
+                    System.Console.WriteLine("Five");
+                    goto A_label;
+                default:
+                    System.Console.WriteLine("Default");
+                    break;
+            }
+
+        }
+    }
+}
