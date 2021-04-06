2a5c6317519122eb2daa0dc107ddab44f6742192
Oliver Zott <zott_oliver@web.de>
2021-04-06 16:35:01 +0200

    Implementing first unit test

--- Chapter04/CalculatorLib/Calculator.cs
+++ Chapter04/CalculatorLib/Calculator.cs
@@ -2,12 +2,12 @@
 
 namespace CalculatorLib
 {
-    public class Class1
+    public class Calculator
     {
 
         public double Add(double a, double b)
         {
-            return a * b;
+            return a + b;
         }
     }
 }
