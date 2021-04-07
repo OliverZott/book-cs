ddcaf4302dccf54601292ad4ec7f9aa4c3e2c7df
Oliver Zott <zott_oliver@web.de>
2021-04-07 20:32:19 +0200

    Oop Example with: classlib, namespace, vs code csproj files

---
+++ Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Example.Shared;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Lena = new();
+            Console.WriteLine("Person: " + Lena);
+        }
+    }
+}
